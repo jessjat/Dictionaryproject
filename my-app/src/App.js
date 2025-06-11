7b93b70a645ae874f65c3043a7b836e2aa01431e
jessjat <mutanujat@gmail.com>
2025-06-11 15:01:39 +0300

    Added the search engine  and an image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,22 @@
-
+import girlpic from "./girlpic.jpg";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> </header>
-      Hello
+      <div className="container">
+      <header className="App-header">
+        <img src={girlpic}className="App-pic img-fluid"alt="pic"/>
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Jacinta Musau</small>
+      </footer>
     </div>
+    </div> 
   );
 }
 
